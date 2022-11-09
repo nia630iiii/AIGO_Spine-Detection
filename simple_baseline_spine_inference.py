a5bcdc94c6c3d2a43169fc5d72eb8bbc08a76f67
ShuFang <503281@gmail.com>
2022-11-09 11:21:15 +0800

    updata Inference

--- simple_baseline_spine_inference.py
+++ simple_baseline_spine_inference.py
@@ -153,13 +153,14 @@ def inference(image_path):
     center, scale = img.center, img.scale
     ans, score = get_final_preds(heatmap_list, center, scale)
     
+    spine_joints = ans
 
     plt.figure(figsize=(10,10))
     plt.imshow(cv2.cvtColor(img.img,cv2.COLOR_BGR2RGB))
     plt.plot(ans[0][:,0], ans[0][:,1], 'ro')
     plt.show()
 
-    print(ans)
+    return spine_joints,img
     
     
         
