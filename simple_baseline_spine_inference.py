4f2b0783f9c9f2627e3c07fcdfa904a30348375f
ShuFang <503281@gmail.com>
2022-11-09 14:54:05 +0800

    test return img

--- simple_baseline_spine_inference.py
+++ simple_baseline_spine_inference.py
@@ -156,9 +156,12 @@ def inference(image_path):
     joint_arr = np.array(joint)
 
     plt.figure(figsize=(10,10))
+    img = cv2.cvtColor(img.img,cv2.COLOR_BGR2RGB)
+    img = plt.plot(ans[0][:,0], ans[0][:,1], 'ro')
+    """plt.figure(figsize=(10,10))
     plt.imshow(cv2.cvtColor(img.img,cv2.COLOR_BGR2RGB))
     plt.plot(ans[0][:,0], ans[0][:,1], 'ro')
-    plt.show()
+    plt.show()"""
 
     return joint_arr,img
 
