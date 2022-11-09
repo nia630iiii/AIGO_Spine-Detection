77184db19b8a2d4b835362cba59fbbdc51e1ae99
ShuFang <503281@gmail.com>
2022-11-09 11:56:23 +0800

    updata Inference

--- simple_baseline_spine_inference.py
+++ simple_baseline_spine_inference.py
@@ -161,10 +161,6 @@ def inference(image_path):
 
     spine_img = draw_human_joints(img, ans)
     
-    """plt.figure(figsize=(10,10))
-    plt.imshow(cv2.cvtColor(img.img,cv2.COLOR_BGR2RGB))
-    plt.plot(ans[0][:,0], ans[0][:,1], 'ro')
-    plt.show()"""
 
     return ans,spine_img 
     
